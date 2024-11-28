04a154accf79b4627208e88abdab6d5c2d647502
jsantorek <jakub.santorek@gmail.com>
2024-11-28 14:15:38 +0100

    Create ArcOptions.hpp

---
+++ src/ArcOptions.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ArcDPS
+{
+    struct Options
+    {
+        unsigned int global_mod1;
+        unsigned int global_mod2;
+        unsigned int global_options;
+    };
+}
